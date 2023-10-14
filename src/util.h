19a14c51f700789bac5651d9f33ebd321a55ebc4
Liwinux <liwinux@Who-knows>
2023-10-14 15:58:45 +0200

    Added utils functions

---
+++ src/util.h
@@ -0,0 +1,11 @@
+#ifndef __UTIL_H_
+#define __UTIL_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+int check_is_number(const char *string);
+
+#endif // __UTIL_H_
